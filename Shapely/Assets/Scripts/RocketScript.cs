d07fca290ec797405e67e928b3bc1a58f54692da
MBenoit9 <mardenbenoit@yahoo.com>
2015-03-20 16:34:48 -0500

    Added Rocket Ship and Moon to the background

---
+++ Shapely/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketScript : MonoBehaviour {
+
+	public float speed = 2.0f;
+	
+	void Awake()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2(-1 * speed, 0);
+	}
+	
+	void Update () {
+		if(transform.position.x < -46)
+		{
+			Debug.Log("here");
+			transform.position = new Vector3(47f, transform.position.y, 0);
+		}
+	}
+}
